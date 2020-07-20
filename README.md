935a08c0bc28ef15ccc3e0ca6f6c97b3e5a83f38
Bruce Li <brucel@netflix.com>
2020-07-19 17:47:48 -0700

    More collection type template variable examples, with readme update.

--- README.md
+++ README.md
@@ -10,3 +10,13 @@ Use `{{.}}` in the template, which will be substituded by the pass-in param when
 For example, `temp.ExecuteTemplate(os.Stdout, "basic_template.gohtml", "Bruce")` will supply value "Bruce" to the occurrence of `{{.}}` in the template
 
 Note, can only pass in one single piece of data into the template via the variable. 
+
+## Parse the templates just once, with pointer variable
+
+**Pointers** in Go programming language or Golang is a variable which is used to store the memory address of another variable. Pointers in Golang is also termed as the special variables. The variables are used to store some data at a particular memory address in the system.
+
+To avoid parsing templates over and over, we can define the `init()` function to parse the whole template folder as a glob, and store in the pointer variable. See the updated `main` function in this repo.
+
+## Skip rendering comment section in template
+
+See https://discourse.gohugo.io/t/how-to-add-comments-in-a-template/75/2 , wrap the comment block with `{{/*   */}}`
--- templates/main.go
+++ templates/main.go
@@ -6,18 +6,47 @@ import (
 	"text/template"	
 )
 
+var temp *template.Template
+
+func init() {
+	temp = template.Must(template.ParseGlob("./*.gohtml"))
+}
+
+// Note: Must use capitalized field name, otherwise can't access from template
+type Player struct {
+	Fname string
+	Lname string
+	Position string
+}
+
 func main() {
 	// temp, err := template.ParseFiles("basic_template.gohtml", "template_with_collection_variable.gohtml")
-	temp, err := template.ParseGlob("./*.gohtml")
-
-	if err != nil {
-		log.Fatalln(err)
-	}
+	// temp := template.Must(template.ParseGlob("./*.gohtml"))
 
 	// err = temp.Execute(os.Stdout, "Bruce")
-	err = temp.ExecuteTemplate(os.Stdout, "basic_template.gohtml", "Bruce")
+	err := temp.ExecuteTemplate(os.Stdout, "basic_template.gohtml", "Bruce")
 
 	if err != nil {
 		log.Fatalln(err)
 	}
+
+	// slice definition example
+	// names := []string{"Harden", "Durant", "James", "Jordan", "Paul"}
+
+	// map definition example
+	// names := map[string]string{
+	// 	"James": "Harden", 
+	// 	"Kevin": "Durant", 
+	// 	"LeBron": "James", 
+	// 	"Michael": "Jordan", 
+	// 	"Chris": "Paul",
+	// }
+
+	// struct definition example
+	players := []Player {
+		Player{"LeBron", "James", "PF"},
+		Player{"Kevin", "Durant", "SF"},
+	}
+
+	err = temp.ExecuteTemplate(os.Stdout, "template_with_collection_variable.gohtml", players)
 }
